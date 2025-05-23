21bd978aaf038116b7d34a6e154777dba652e63a
PowerX-NOT <pugalg100@gmail.com>
2025-05-23 21:15:27 +0530

    fix exuction error
    power@not:~/intern/parking-vision$ python3 src/main.py '/home/power/intern/input.jpg'
    Traceback (most recent call last):
      File "/home/power/intern/parking-vision/src/main.py", line 4, in <module>
        from slot_detection import detect_slots
    ImportError: cannot import name 'detect_slots' from 'slot_detection' (/home/power/intern/parking-vision/src/slot_detection.py)

--- src/slot_detection.py
+++ src/slot_detection.py
@@ -1,5 +1,4 @@
 import torch
-import numpy as np
 
 class YOLOv5SlotDetector:
     def __init__(self, weights="yolov5s.pt", device=None):
@@ -9,15 +8,10 @@ class YOLOv5SlotDetector:
         self.model.eval()
 
     def detect_slots(self, frame):
-        """
-        Detect cars as 'slots' (occupied) using YOLOv5.
-        Returns a list of bounding boxes (x, y, w, h) for each detected car.
-        """
         results = self.model(frame)
         slots = []
         for *xyxy, conf, cls in results.xyxy[0].cpu().numpy():
-            # COCO car class is 2, truck is 7, bus is 5, motorcycle is 3, etc.
-            if int(cls) in [2, 3, 5, 7]:
+            if int(cls) in [2, 3, 5, 7]:  # car, motorcycle, bus, truck
                 x1, y1, x2, y2 = map(int, xyxy)
                 w, h = x2 - x1, y2 - y1
                 slots.append((x1, y1, w, h))
